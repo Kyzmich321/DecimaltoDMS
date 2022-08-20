630765682ef97ada03394f61293ff372a69074fc
Dima Kyryliuk <98225149+Kyzmich321@users.noreply.github.com>
2022-08-20 11:03:24 +0300

    Update DecimaltoDMS.py

--- DecimaltoDMS.py
+++ DecimaltoDMS.py
@@ -1,9 +1,7 @@
 def DecimaltoDMS(Decimal):
-   d = int(Decimal)
-   m = int((Decimal - d) * 60)
-   if d >= 0:
-      return f'{abs(d)}^{abs(m)}E'
-   else:
-      return f'{abs(d)}^{abs(m)}W'
+    d = int(Decimal)
+    m = int((Decimal - d) * 60)
+    var = 'E' if d >= 0 else 'W'
+    return f'{abs(d)}^{abs(m)}{var}'
 
 
